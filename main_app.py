0414e6c3755596dba7fff022a74fd0ee896fd414
Anshul Pandey <anshulpandey0606@gmail.com>
2022-08-07 02:31:48 +0530

    Change url

--- main_app.py
+++ main_app.py
@@ -19,7 +19,7 @@ def home():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
     })
     s.headers = headers
-    data = s.get(f"https://{address}/rest/v1/offers?camelCase=1&user_badges%5B0%5D=7&user_badges%5B1%5D=8&user_badges%5B2%5D=2&user_badges%5B3%5D=1&user_badges%5B4%5D=5&withFavorites=false&crypto_currency_id=1&is_payment_method_localized=0&visitor_country_has_changed=false&visitor_country_iso=IN&payment-method%5B0%5D=with-any-payment-method&type=sell", headers=headers, verify=False).text
+    data = s.get(f"https://{address}/rest/v1/offers?camelCase=1&type=sell", headers=headers, verify=False).text
     #data=requests.get(url, headers=headers).content.decode()
     data=json.loads(data)
 
